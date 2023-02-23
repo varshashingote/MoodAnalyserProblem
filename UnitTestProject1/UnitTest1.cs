07282aa43b95ffbc2893540b4173112508fa5fe3
Gunjal Varsha Bhima <120667268+varshashingote@users.noreply.github.com>
2023-02-23 08:03:25 +0530

    Update UnitTest1.cs

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -8,17 +8,18 @@ namespace UnitTestProject1
 {
     [TestClass]
     public class MoodAnalyerTestClass
-
     {
+        //TC2-I am in any mood should return HAPPY
         [TestMethod]
-        public void Given_Message_Should_Return_UserMethod()
+        public void GivenAnyMoodRetuenHAPPY()
         {
-           
+           //arrange
             String message = "I am in happy mood";
             String exceptedValue = "HAPPY";
             MoodAnalyser mood = new MoodAnalyser(message);
             //Act
             String result = mood.AnalyseMood(message);
+            //assert
             Assert.AreEqual(exceptedValue, result);
         }
 
@@ -26,17 +27,16 @@ namespace UnitTestProject1
         [TestMethod]
         public void GivenSadMoodMessage_WhenAnalyse_ShouldReturnSAD()
         {
-
+            // i am sad mood should return SAD
+            //arrange
             string message = "I am sad mood";
             string expectedValue = "SAD";
             MoodAnalyser moodAnalyser = new MoodAnalyser();
-
+             //act
             string result = moodAnalyser.AnalyseMood(message);
-
+            //assert
             Assert.AreEqual(expectedValue, result);
         }
-
-
 
     }
 }
